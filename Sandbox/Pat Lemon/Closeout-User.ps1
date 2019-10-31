64aa65b789b3000c845f98483d1a15c589f12ecc
peaayetee <57104715+peaayetee@users.noreply.github.com>
2019-10-30 19:37:18 -0700

    clean up

--- Sandbox/Pat Lemon/Closeout-User.ps1
+++ Sandbox/Pat Lemon/Closeout-User.ps1
@@ -3,68 +3,23 @@
     [string]$UserID,
     [string]$ServNowNumber
     )
-#Sets the $TechName var to the logged in users display name. Need to work on removing "- RA"
+#Sets the $TechName var to the logged in users display name. 
+#Need to work on removing "- RA"
 $TechName = ([adsi]"WinNT://$env:userdomain/$env:username,user").fullname
-#Get todays date in [yyyy-mm-dd] format
 $Today = (Get-Date -UFormat "%Y-%m-%d")
+$Note = "Restrictions set on $Today by $TechName, Service-Now: $ServNowNumber"
 
-function Set-RestrictNote {
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
-        [string]$UserID,
-        [string]$ServNowNumber
-        )
-#End of Parameters
-    Process {
-    $Note = "Restrictions set on $Today by $TechName, Service-Now: $ServNowNumber"
-    #Write the notes to the Description field on the General tab
-    Write-Host -ForegroundColor Green "Note added to description field: `n$Note"
-    Set-ADUser -Identity $UserID -Description $Note
-    }
-#End of Process
-}<#  #>
-function Get-Grouplist {
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
-        [string]$UserID
-        )
-#End of Parameters
-    Process {
-    #Collect list of AD Group Memberships
-    #Need to remove "Domain Users from $GroupList
-    Write-Host -ForegroundColor Green "Collecting list of group memberships for $UserID"
-    $GroupList = (Get-ADPrincipalGroupMembership -Identity $UserID -Server (Get-ADDomain).PDCEmulator).Name
-    Write-Host -ForegroundColor Green "List of groups collected for $UserID"
-    Write-Output $GroupList
-    }
-#End of Process
+#Add the Restrictions note to the users AD
+Set-ADUser -Identity $UserID -Description $Note
+
+#Get the list of group memberships for the user
+$GroupList = (Get-ADPrincipalGroupMembership -Identity $UserID -Server (Get-ADDomain).PDCEmulator).Name
+#Remove "Domain Users" from the GroupList
+$GroupList = @($GroupList | Where-Object { $_ =ne "Domain Users"})
+
+#Add List of groups to Telephone notes
+Set-ADUser -Identity $UserID -Replace @{info="$GroupList"}
+
+foreach ($Group in $GroupList) {
+    Remove-ADGroupMember -Identity $Group -Members $UserID
 }
-
-function Set-GrouplistNote {
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
-        [string[]]$GroupList,
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
-        [string]$UserID
-        )
-#End of Parameters
-    Process {
-        foreach ($Group in $GroupList) {
-            Set-ADUser -Identity $UserID -Replace @{info="$GroupList"}
-        }
-    }
- }
-
-function Remove-Memberships {
-    [CmdletBinding()]
-    param(
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
-        [string[]]$GroupList,
-        [parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$true)]
-        [string]$UserID
-        #End of Parameters
-    Process {
-
