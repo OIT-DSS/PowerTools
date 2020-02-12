fb45d389d9e2ed99aa5d1171c658e470f37756f2
Benedict Chua <services@bentograph.com>
2020-02-12 08:01:35 -0800

    Reorganized Folder Structure

--- Sandbox/Benedict Yi Chua/Presenter-Create.ps1
+++
@@ -1,4 +0,0 @@
-$Password = Read-Host -AsSecureString -Prompt
-New-LocalUser "Presenter" -Password $Password -FullName "Presenter" -Description "Presenter"
-Add-LocalGroupMember -Group "Users" -Member "Presenter"
-Restart-Computer
